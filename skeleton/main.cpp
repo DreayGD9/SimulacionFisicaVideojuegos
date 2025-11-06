8b8756fbea8ff0b7d93932906d74ced73605d57a
DreayGD9 <157888872+DreayGD9@users.noreply.github.com>
2025-11-06 17:38:43 +0100

    Entrega proyecto intermedio (arreglada)

--- skeleton/main.cpp
+++ skeleton/main.cpp
@@ -127,16 +127,6 @@ void keyPress(unsigned char key, const PxTransform& camera)
 	{
 		game->enemiesFire();
 		break;
-	
-	case 'Z':
-	{
-		game->togglePlrSpeed();
-		break;
-	}
-	case 'X':
-	{
-		game->toggleEnmSpeed();
-		break;
 	}
 	case 'Z':
 	{
